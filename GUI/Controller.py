bd12e8cda801695e3c815acec2ff6c83a89f44c4
Kai Hang Chen <40642739+Kaihchen1230@users.noreply.github.com>
2018-11-30 14:15:29 -0500

    new GUI (#5)
    * Delete login.py
    
    * Delete login.ui
    
    * Delete signup.py
    
    * Delete signup.ui
    
    * v1.1

---
+++ GUI/Controller.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5 import QtCore, QtWidgets
+from GUI.login import Login
+from GUI.Signup import Signup
+
+
+class Controller:
+
+    def __init__(self):
+        pass
+
+    def show_login(self):
+        self.login = Login()
+        if self.login.signup_event():
+            self.login.switch_window.connect(self.show_signup)
+
+        else:
+            print("not a new accont")
+        self.login.show()
+
+    def show_signup(self):
+        self.login.email_field.setText("")
+        self.login.password_field.setText("")
+        self.window = Signup()
+        self.window.show()
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    controller = Controller()
+    controller.show_login()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
